e0dd30b773cd9a1243e7ea8e6a38fbc476fee5ce
NCrashed <NCrashed@gmail.com>
2015-12-13 14:06:43 +0300

    Fix converting from BLP JPEG with alpha

--- blp2any/Main.hs
+++ blp2any/Main.hs
@@ -51,7 +51,7 @@ testFile inputFile isDir outputPath = do
     ImageCMYK8 _ ->  "ImageCMYK8"
     ImageCMYK16 _ ->  "ImageCMYK16"
   -}
-  
+
   res <- writeDynamicPng outputFile $ toPngRepresentable img
   case res of 
     Left err -> fail $ inputFile ++ ": " ++ err 
@@ -70,8 +70,8 @@ toPngRepresentable i = case i of
   ImageRGBA8 _ -> i 
   ImageRGBA16 _ -> i
   ImageYCbCr8 p -> ImageRGB8 . convertImage $ p
-  ImageCMYK8 p -> ImageRGB8 . convertImage $ p 
-  ImageCMYK16 p -> ImageRGB16 . convertImage $ p
+  ImageCMYK8 p -> ImageRGBA8 . convertCMYK8Image $ p 
+  ImageCMYK16 p -> ImageRGBA16 . convertCMYK16Image $ p
 
 convertFloatImage :: Image PixelRGBF -> Image PixelRGB16
 convertFloatImage = pixelMap convert 
@@ -80,3 +80,31 @@ convertFloatImage = pixelMap convert
       (round $ 65535 * rf)
       (round $ 65535 * gf)
       (round $ 65535 * bf)
+
+convertCMYK8Image :: Image PixelCMYK8 -> Image PixelRGBA8 
+convertCMYK8Image = pixelMap convert 
+  where 
+  convert (PixelCMYK8 c m y k) =
+      PixelRGBA8 (clampWord8 r) (clampWord8 g) (clampWord8 b) k
+    where
+    clampWord8 = fromIntegral . max 0 . min 255 . (`div` 255)
+
+    ik :: Int
+    ik = fromIntegral k
+    r = fromIntegral y * ik 
+    g = fromIntegral m * ik 
+    b = fromIntegral c * ik 
+
+convertCMYK16Image :: Image PixelCMYK16 -> Image PixelRGBA16 
+convertCMYK16Image = pixelMap convert 
+  where 
+  convert (PixelCMYK16 c m y k) =
+      PixelRGBA16 (clampWord16 r) (clampWord16 g) (clampWord16 b) 65535
+    where
+    clampWord16 = fromIntegral . max 0 . min 65535 . (`div` 65535)
+
+    ik :: Int
+    ik = fromIntegral k
+    r = fromIntegral y * ik 
+    g = fromIntegral m * ik 
+    b = fromIntegral c * ik 
